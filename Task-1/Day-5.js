619a68c35cf5df3074284d65282b5a6fd6067ce2
Amrutasai <46530658+Amrutasai@users.noreply.github.com>
2019-07-18 23:03:30 +0530

    Update Day-5.js

--- Task-1/Day-5.js
+++ Task-1/Day-5.js
@@ -1,1 +1,79 @@
+let a={
+        fname:'Lyanna',
+        lname:'Mormon',
+        seasonsOfExperience:3,
+        firstSeen:"The Broken Man",
+        latestEpi:"The Last of the Starks",
+        episodesOfExperience: 9, 
+        titles:'Lady of Bear Island',
+        status:'Deceased',
+        age:13,
+        DOB:'292 AC',
+        death:'305 AC at Winterfell, Crushed by a wight giant during the Battle of Winterfell, whom she managed to destroy before succumbing to her wounds',
+        origin:'Bear Island, the North',
+        allegiance:{
+            birth:'House Mormont',
+            oath:'House Stark',
+            death:'White Walkers'},
+        predecessor:'Maege Mormont',
+        religion:'Old Gods of the Forest',
+        mother:'Maege Mormont',
+        portrayedBy:'Bella Ramsey',
+        noOfAwards:4,
+        getFullName(){
+            return `${this.fname} ${this.lname}`;
+        },    
+        getBio(){
+            return `Lady ${this.getFullName()} [birth: ${this.DOB}] was the ${this.titles} and thus the head of ${this.allegiance.birth} of ${this.origin} ever since the death of her mother, ${this.mother}. She pledged ${this.allegiance.birth} forces to ${this.allegiance.oath} upon meeting Jon Snow, Sansa Stark, and Davos Seaworth, making her house one of the few loyalists to fight for the Starks against House Bolton at the Battle of the Bastards. She was killed in ${this.death} at the age of ${this.age}.`
+        },
+        getCharacterInfo(){
+            return `We are introduced to ${this.getFullName()} in '${this.firstSeen}' as the ${this.titles}, successor of ${this.predecessor} who was also her mother. She is last seen in '${this.latestEpi}' fighting alongside ${this.allegiance.death}. She was buried as per her religion ${this.religion}.`
+        },
+        getActressInfo(){
+            return `${this.portrayedBy} played the role of ${this.getFullName()} in the show Game of Thrones. We meet her character in '${this.firstSeen}' and bid her goodbye in '${this.latestEpi}'.She gained an experience of ${this.episodesOfExperience} over ${this.seasonsOfExperience} seasons. Her talent as an actress was recognised when she was nominated for ${this.noOfAwards} awards.`
+         }
+}
+console.log('FIRST NAME: '+a.fname);
+console.log('FULL NAME: '+a.getFullName());
+console.log('BIO: \n'+a.getBio());
+console.log('CHARACTER INFORMATION: \n'+a.getCharacterInfo());
+console.log('ACTRESS INFORMATION: \n'+a.getActressInfo());
+console.log('NO. OF YEARS: '+a.seasonsOfExperience+' \nNO. OF EPISODES: '+a.episodesOfExperience);
 
+let b={
+        fname:'Sansa',
+        lname:'Stark',
+        seasonsOfExperience:8,
+        firstSeen:"Winter Is Coming",
+        latestEpi:"The Iron Throne",
+        episodesOfExperience: 59, 
+        titles:['Princess of Winterfell','Lady of Winterfell','Queen in the North'],
+        status:'Alive',
+        age:20,
+        DOB:'285 AC',
+        death:'NA',
+        origin:'Winterfell',
+        allegiance:{
+            birth:'House Stark',
+            oath:'House Targaryen',
+            marriage:['House Bolton','House Lannister' ]
+            },
+        predecessor:'Jon Snow',
+        religion:'Old Gods of the Forest',
+        mother:'Catelyn Stark',
+        portrayedBy:'Sophie Turner',
+        noOfAwards:4,
+        getFullName(){
+            return `${this.fname} ${this.lname}`;
+        },    
+        getBio(){
+            return `Lady ${this.getFullName()} [birth: ${this.DOB}] was the ${this.titles[0]} and thus the head of ${this.allegiance.birth} of ${this.origin} ever since the death of her mother, ${this.mother}. She pledged ${this.allegiance.birth} forces to ${this.allegiance.oath} upon meeting Jon Snow, Sansa Stark, and Davos Seaworth, making her house one of the few loyalists to fight for the Starks against House Bolton at the Battle of the Bastards. She was killed in ${this.death} at the age of ${this.age}.`
+        },
+        getCharacterInfo(){
+            return `We are introduced to ${this.getFullName()} in '${this.firstSeen}' as the ${this.titles[0]}, successor of ${this.predecessor} who was also her mother. She is last seen in '${this.latestEpi}' fighting alongside ${this.allegiance.death}. She was buried as per her religion ${this.religion}.`
+        },
+        getActressInfo(){
+            return `${this.portrayedBy} played the role of ${this.getFullName()} in the show Game of Thrones. We meet her character in '${this.firstSeen}' and watch her become ${this.titles[2]} in '${this.latestEpi}'.She gained an experience of ${this.episodesOfExperience} over ${this.seasonsOfExperience} seasons. Her talent as an actress was recognised when she was nominated for ${this.noOfAwards} awards.`
+         }
+
+}
