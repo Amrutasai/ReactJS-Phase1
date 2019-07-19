3d1be8944766500fd596a90126f6d169797498f5
Amrutasai <46530658+Amrutasai@users.noreply.github.com>
2019-07-19 20:33:35 +0530

    Update Day-5.js

--- Task-1/Day-5.js
+++ Task-1/Day-5.js
@@ -48,7 +48,7 @@ let b={
         firstSeen:"Winter Is Coming",
         latestEpi:"The Iron Throne",
         episodesOfExperience: 59, 
-        titles:['Princess of Winterfell','Lady of Winterfell','Queen in the North','Lady Regent of the North '],
+        titles:['Princess of Winterfell','Lady of Winterfell','Queen in the North','Lady Regent of the North'],
         status:'Alive',
         marriage:['Tyrion Lannister','Ramsay Bolton'],
         age:20,
@@ -69,19 +69,20 @@ let b={
             return `${this.fname} ${this.lname}`;
         },    
         getBio(){
-            return `Lady ${this.getFullName()} [birth: ${this.DOB}] was the ${this.titles[0]} born to ${this.mother}. She was wedded to ${this.marriage[0]} and hence became ${this.titles[3]}. After knowing the truth she decides to return to ${this.origin} and becomes ${this.titles[1]} after her marriage with ${this.marriage[1]}.Later,${this.fname} declares the North an independent kingdom and is later crowned the ${this.title[2]}and rule the north.`
+            return `Lady ${this.getFullName()} [birth: ${this.DOB}] was the ${this.titles[0]} born to ${this.mother}. She was wedded to ${this.marriage[0]} and hence became ${this.titles[3]}. After knowing the truth she decides to return to ${this.origin} and becomes ${this.titles[1]} after her marriage to ${this.marriage[1]}. Later,${this.fname} declares the North an independent kingdom and is later crowned the ${this.titles[2]} and rules the north.`
         },
         getCharacterInfo(){
-            return `We are introduced to ${this.getFullName()} in '${this.firstSeen}' as the ${this.titles[0]}, step-sister of ${this.predecessor} who was also her cousin. She is last seen in '${this.latestEpi}' being crowned as ${this.title[2]} as per her religion ${this.religion}.`
+            return `We are introduced to ${this.getFullName()} in '${this.firstSeen}' as the ${this.titles[0]}, step-sister of ${this.predecessor} who was also her cousin. She is last seen in '${this.latestEpi}' being crowned as ${this.titles[2]} as per her religion ${this.religion}.`
         },
         getActressInfo(){
-            return `${this.portrayedBy} played the role of ${this.getFullName()} in the show Game of Thrones. We meet her character in '${this.firstSeen}' and watch her become ${this.titles[2]} in '${this.latestEpi}'.She gained an experience of ${this.episodesOfExperience} over ${this.seasonsOfExperience} seasons. Her talent as an actress was recognised when she was nominated for ${this.noOfAwards} awards.`
+            return `${this.portrayedBy} played the role of ${this.getFullName()} in the show Game of Thrones. We meet her character in '${this.firstSeen}' and watch her become ${this.titles[2]} in '${this.latestEpi}'.She gained an experience of ${this.episodesOfExperience} over ${this.seasonsOfExperience} seasons. Her talent as an actress was recognised when she was nominated for ${this.noOfAwards} award.`
          }
 
 }
-console.log('FIRST NAME: '+b.fname);
-console.log('FULL NAME: '+b.getFullName());
-console.log('BIO: \n'+b.getBio());
-console.log('CHARACTER INFORMATION: \n'+b.getCharacterInfo());
-console.log('ACTRESS INFORMATION: \n'+b.getActressInfo());
-console.log('NO. OF YEARS: '+b.seasonsOfExperience+' \nNO. OF EPISODES: '+b.episodesOfExperience);
+
+ console.log('FIRST NAME: '+b.fname);
+ console.log('FULL NAME: '+b.getFullName());
+ console.log('BIO: \n'+b.getBio());
+ console.log('CHARACTER INFORMATION: \n'+b.getCharacterInfo());
+ console.log('ACTRESS INFORMATION: \n'+b.getActressInfo());
+ console.log('NO. OF YEARS: '+b.seasonsOfExperience+' \nNO. OF EPISODES: '+b.episodesOfExperience);
