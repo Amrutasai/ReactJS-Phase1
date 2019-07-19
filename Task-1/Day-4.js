ee1b9b4d1da09896846532145f7590ce5f6d4473
Amrutasai <46530658+Amrutasai@users.noreply.github.com>
2019-07-19 20:42:30 +0530

    Update Day-4.js

--- Task-1/Day-4.js
+++ Task-1/Day-4.js
@@ -15,25 +15,31 @@ console.log(a);
 console.log('e) Add an element(999) at the beginning of Array a:');
 a=[...[999],...a];
 console.log(a);
-
+console.log('f.	Find the index of number 17 in the array.');
 a.map((index)=>
 {
         a[index]===17?console.log(index):{};
 });
+console.log('g.	Remove the item at index 11 from the array:');
 a.splice(10,1);
 console.log(a);
+console.log('h.	Remove 5 items starting from index 4:');
 a.splice(3,5);
 console.log(a);
+console.log('i.	Create a new array, b, having a multiplication of number and its index from array a:');
 let b=a.map((value,index)=>
 {
         return value*index;
 });
 console.log(b);
+console.log('j.	Extract numbers at index 7, 8, 9,10 from array a:');
 console.log(a.slice(7,11));
+console.log('k.	Create an array, c, from array b, of numbers which are greater than 30:');
 let c=b.filter((value)=>
 {
     return value>30;
 });
 console.log(c);
+console.log('l.	Merge all the 3 arrays, a, b, c:');
 let d=[...a,...b,...c];
 console.log(d);
